402b940c6fa26617746a8ae709a275716082e5e9
Num881 <ibabenko894@gmail.com>
2025-04-17 19:52:00 +0300

    Delete solutions/solutions directory

--- solutions/solutions/5-high-order-functions.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-
-// BEGIN
-const takeOldest = (arr, count = 1) => {
-    let newArr = arr.sort((A, B) => Date.parse(A.birthday) - Date.parse(B.birthday));
-    return newArr.slice(0, count);
-}
-export default takeOldest
-// END
